c6e568da1538febc14ceec3763e6442355118d98
jkmacy <joyousjake.jm@gmail.com>
2018-10-22 12:44:31 -0500

    Reads user input for multiplication table size, then prints it formatted
    Runs in the console, with the input printed within a formatted string

--- src/main/kotlin/com/joyousjake/flashCards/Main.kt
+++ src/main/kotlin/com/joyousjake/flashCards/Main.kt
@@ -1,5 +1,29 @@
 package com.joyousjake.flashCards
 
-fun main(args: Array<String>) {
 
+fun main(args: Array<String>) {
+    print("Enter the size of the multiplication table (Default: 12; 12x12) ")
+    val table = readInt()
+    print("The size of the table is $table by $table")
+
+}
+
+fun readInt(): Int {
+    val s = readLine()
+    val i: String = when  {
+        s == null -> "12"
+        Regex("[a-zA-Z]*").containsMatchIn(s) -> "12"
+        else -> s
+    }
+    var r: Int
+    do {
+        try {
+            r = i.toInt()
+        }
+        catch (e: NumberFormatException) {
+            continue
+        }
+        break
+    } while (true)
+    return r
 }
