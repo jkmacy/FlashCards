1270c8b1baa216c7b051c266303c6dd008684032
jkmacy <joyousjake.jm@gmail.com>
2018-10-22 13:55:57 -0500

    Fixed bug preventing anything besides 12x12 multiplication table

--- src/main/kotlin/com/joyousjake/flashCards/Main.kt
+++ src/main/kotlin/com/joyousjake/flashCards/Main.kt
@@ -10,20 +10,8 @@ fun main(args: Array<String>) {
 
 fun readInt(): Int {
     val s = readLine()
-    val i: String = when  {
-        s == null -> "12"
-        Regex("[a-zA-Z]*").containsMatchIn(s) -> "12"
-        else -> s
+    return when  {
+        s == null || Regex("[^0-9]*").matches(s) -> 12
+        else -> s.toInt()
     }
-    var r: Int
-    do {
-        try {
-            r = i.toInt()
-        }
-        catch (e: NumberFormatException) {
-            continue
-        }
-        break
-    } while (true)
-    return r
 }
