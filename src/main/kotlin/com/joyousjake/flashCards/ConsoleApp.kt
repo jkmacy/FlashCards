150098e1a0861a7d9b683536575c528d9f42a96b
jkmacy <joyousjake.jm@gmail.com>
2018-10-23 19:38:20 -0500

    Now asks for solutions to equations from multiplication table
    Equations are composed of two pseudo-randomly-generated, integer terms.
    The user, after answering the question, can choose whether to solve
    another equation, create a new multiplication table, or quit.

--- src/main/kotlin/com/joyousjake/flashCards/ConsoleApp.kt
+++ src/main/kotlin/com/joyousjake/flashCards/ConsoleApp.kt
@@ -1,16 +1,63 @@
 package com.joyousjake.flashCards
 
+const val tablePrompt = "Enter the size of the multiplication table [Default - 12 (12x12); table size should be min of 3]: "
 
 fun main(args: Array<String>) {
-    print("Enter the size of the multiplication table (Default: 12; 12x12) ")
-    val table = MultiplicationTable(readInt())
-    println(table)
+    print(tablePrompt)
+    var size = readInt()
+    while (size < 3) {
+        print("The table size should at least be 3: ")
+        size = readInt()
+    }
+
+    var table = MultiplicationTable(size)
+    println("$table\n")
+
+    var promptAnswer: Char
+    loop@ while (true) {
+        val (a, b) = Pair(table.randomInteger, table.randomInteger)
+        val correctAnswer = table.product(a, b)
+        print("$a x $b = ")
+        val yourAnswer = readInt()
+        println()
+
+        if (yourAnswer == correctAnswer)
+            println("Your answer was correct!")
+        else
+            println("Sorry, the correct answer was $correctAnswer")
+
+        print("""
+            |Do you want to answer another problem?
+            |[Y (Yes), n (no), m (new multiplication table)]
+        """.trimMargin() + " ")
+        val line = readLine()
+        promptAnswer = if (line != null && line.isNotEmpty()) line[0].toLowerCase() else 'y'
+        println()
+
+        when (promptAnswer) {
+            'y' -> continue@loop
+            'n' -> {
+                println("Exiting program...")
+                break@loop
+            }
+            'm' -> {
+                print(tablePrompt)
+                table = MultiplicationTable(readInt())
+                println("$table\n")
+                continue@loop
+            }
+            else -> {
+                println("Entered an invalid option, exiting program...")
+                break@loop
+            }
+        }
+    }
 }
 
 fun readInt(): Int {
     val s = readLine()
     return when  {
-        s == null || Regex("[^0-9]*").matches(s) -> 12
+        s == null || Regex("[^0-9]*").matches(s) -> -1
         else -> s.toInt()
     }
 }
--- src/main/kotlin/com/joyousjake/flashCards/MultiplicationTable.kt
+++ src/main/kotlin/com/joyousjake/flashCards/MultiplicationTable.kt
@@ -1,11 +1,16 @@
 package com.joyousjake.flashCards
 
+import java.util.Random
 
 class MultiplicationTable(val size: Int) {
 
-    fun product(a: Int, b: Int) = if (a in 1..size && b in 1..size) a * b
+    fun product(a: Int, b: Int) = if (a in 2..size && b in 2..size) a * b
         else throw Exception()
+
+    val randomInteger: Int
+        get() = (2..size).random()
 
     override fun toString(): String = "The size of the table is $size by $size"
 
+    fun ClosedRange<Int>.random() = Random().nextInt(endInclusive + 1 - start) + start
 }
